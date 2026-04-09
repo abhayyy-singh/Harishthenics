caae54e1d57e995710477f9669e3f2a32cba7e20
Abhay Singh <98868196+abhayyy-singh@users.noreply.github.com>
2026-04-09 14:26:46 +0530

    added test key

--- personalized-program.js
+++ personalized-program.js
@@ -7,7 +7,7 @@
     'use strict';
 
     const PROGRAM_CONFIG = {
-        razorpayKey: 'rzp_live_RZDqqPc9XD0IjO',
+        razorpayKey: 'rzp_test_RZG0vfhDgIuZYI',
         amount: 1500000,
         currency: 'INR',
         businessName: 'Haristhenics',
