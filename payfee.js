afa7f7796e3de62b265023a794de6178003e42f1
Abhay Singh <itsabhaypvt@gmail.com>
2026-05-09 20:10:55 +0530

    live key aded

--- payfee.js
+++ payfee.js
@@ -11,7 +11,7 @@
     // ==========================================
     const PAYFEE_CONFIG = {
         // Razorpay
-        razorpayKeyId: 'rzp_test_SLz0IZGqiyDc7P',
+        razorpayKeyId: 'rzp_live_SLytjftdf8NDpx',
         currency: 'INR',
         businessName: 'HS FutureWorld',
         description: 'Fee Payment - Haristhenics',
